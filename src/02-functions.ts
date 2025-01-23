f3db787c3c33555bdd1dff635fd2a102493d10d9
zft666zft <2810458566@qq.com>
2025-01-23 19:00:31 +0000

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,2 +1,43 @@
 import {Friend, Colleague } from './myTypes'
 import { friends, colleagues } from "./01-basics";
+
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}` 
+}
+
+function allOlder(friends: Friend[]): string[] {
+    return friends.map(friend => {
+        friend.age += 1;
+        return `${friend.name} is now ${friend.age}`;
+    });
+}
+
+console.log(older(friends[0]))
+console.log(allOlder(friends))
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+    const result = cs.sort(
+      (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+  }
+  console.log(highestExtension(colleagues.current));
+  
+  // Add a colleague to the array with the highest extension number plus 1.
+function addColleague(cs: Colleague[], name: string, department: string, email: string): void {
+    const highestExt = highestExtension(cs).contact.extension;
+    const newColleague: Colleague = {
+        name: name,
+        department: department,
+        contact: {
+            email: email,
+            extension: highestExt + 1
+        }
+    };
+    cs.push(newColleague);
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
