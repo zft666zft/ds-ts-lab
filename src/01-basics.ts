a23094dfb7a0bfc9eda2a2374e06e97146dcb61c
zft666zft <2810458566@qq.com>
2025-01-23 18:40:38 +0000

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -14,6 +14,12 @@ interface Colleague {
     };
 }
 
+interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+  }
+  
+
 const friend1 = {
     name: "Paul Fleming",
     phone: "087-12345",
@@ -56,10 +62,11 @@ const friend1 = {
       extension: 125,
     },
   };
-  const colleagues = {
+  export const colleagues : ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
+  
   
   console.log(colleagues.current[0]);
   
